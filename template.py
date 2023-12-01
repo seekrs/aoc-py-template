424b80f9f248e7741814927c41ff923dbc2f141c
xtrm <oss@xtrm.me>
2023-12-01 02:30:09 +0100

    ✨ feature(template.py): nicen ux

--- template.py
+++ template.py
@@ -6,7 +6,7 @@ necessary boilerplate, imports, and helpers to get you running solving problems.
 Have fun! ~ @kiroussa
 """
 
-import collections; import itertools; import math; import os; import sys; import time; from typing import List, Tuple
+import collections; import datetime; import itertools; import logging; import math; import os; import sys; import time; from typing import List, Tuple
 
 #######
 # Your code goes here!!!! :D
@@ -24,9 +24,11 @@ def solve_part_two(input: List[str]) -> str:
 # Helpers
 def read_input(file: str) -> List[str]:
     """Read the input file and return the list of lines."""
-    with open(file, 'r') as input_file:
-        return input_file.read().splitlines()
-
+    try:
+        with open(file, 'r') as input_file:
+            return input_file.read().splitlines()
+    except FileNotFoundError as e:
+        return [None, e.strerror]
 
 # Main
 def main():
@@ -37,17 +39,44 @@ def main():
 
     To run the script, simply run `python main.py <input_file>` from the command line.
     """
-    logger = logging.getLogger(__name__)
+    folder_name = os.path.dirname(os.path.realpath(__file__))
+    folder_name = folder_name.split(os.sep)[-1]
+    logger = logging.getLogger(folder_name)
 
     if len(sys.argv) < 2:
         logger.error('No input file specified')
         logger.error(f'Usage: python {sys.argv[0]} <input_file>')
         return
     
+    # Logger setup boilerplate
+    class CustomFormatter(logging.Formatter):
+        def format(self, record):
+            timestamp = datetime.datetime.now().strftime('%H:%M:%S')
+            space = ''
+            if record.levelname == 'INFO':
+                space = ' '
+            if record.levelname == 'DEBUG':
+                log_message = f"[{timestamp}] [{record.levelname}/{record.threadName}] ({record.name}:{record.lineno}): {record.getMessage()}"
+            else:
+                log_message = f"[{timestamp}] {space}{record.levelname} ({record.name}): {record.getMessage()}"
+            return log_message
+    sh = logging.StreamHandler()
+    sh.setFormatter(CustomFormatter())
+    sh.setLevel(logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(sh)
+
+    # Read input
     input_to_read = sys.argv[1]
     logger.info(f'Using input file: {input_to_read}')
 
     input = read_input(input_to_read)
+    if not input or not input[0]:
+        if input:
+            logger.error(f'Error reading input file: {input[1]}')
+        else:
+            logger.error('No valid input found, aborting...')
+        return
 
     start_time = time.time()
     part_one = solve_part_one(input)
